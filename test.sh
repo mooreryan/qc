0002871631eced5f120eff8b1f02fb1c3307c3c7
Ryan Moore <moorer@udel.edu>
2016-03-29 14:04:26 -0400

    Add no flash pipeline

--- test.sh
+++ test.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-ruby qc.rb -f test_files/test/six.r1.fq.gz,test_files/test/two.r1.fq.gz,test_files/test2/apple.r1.fq.gz,test_files/test2/pie.r1.fq.gz -r test_files/test/six.r2.fq.gz,test_files/test/two.r2.fq.gz,test_files/test2/apple.r2.fq.gz,test_files/test2/pie.r2.fq.gz
-
-tree test_files
+rm -r test_files/test/qc4_no_flash; pigz test_files/test/* && time ruby qc4_no_flash.rb -s test_files/test/six.r1.fq.gz -i test_files/test/six.r2.fq.gz -r test_files/test/two.r1.fq.gz -w test_files/test/two.r2.fq.gz -t 2 -o test_files/test/qc4_no_flash && tree test_files/test/qc4_no_flash
